4f64bcfe90e1068be55bed47978b7c3ebc3a33a9
joletty <happytk70@gmail.com>
2024-11-04 12:49:36 +0700

    Update historical2022.py

--- historical2022.py
+++ historical2022.py
@@ -12,7 +12,7 @@ def calculate_vwap(df):
 def main():
     df = pd.read_csv('2022_europe.csv')
     df_v = pd.read_csv('2022_vwap.csv')
-    chart = StreamlitChart(width=1100, height=600)
+    chart = StreamlitChart(width=1100, height=1000)
     chart.legend(visible=True)
     
     chart.set(df)    
